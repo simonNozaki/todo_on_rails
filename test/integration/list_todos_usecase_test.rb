4d17e686cb71a82e2bcce3a192ab5a35e2b02b88
snozaki <snozaki@netprotections.co.jp>
2022-11-17 11:18:08 +0900

    MOD: アサーションの微修正

--- test/integration/list_todos_usecase_test.rb
+++ test/integration/list_todos_usecase_test.rb
@@ -35,35 +35,9 @@ class ListTodosUsecaseTest < ActionDispatch::IntegrationTest
 
   test "should have sub todos associated with main user" do
     result = @use_case.execute(users(:archer).id)
-    assert_equal(
-      {
-        items: [
-          {
-            id: @todo_item_main_2.id.to_s,
-            title: @todo_item_main_2.title,
-            state: :in_progress,
-            deadline: @todo_item_main_2.deadline.to_datetime,
-            comment: @todo_item_main_2.comment,
-            sub_todos: [
-              {
-                id: @sub_todo_item_1.id.to_s,
-                title: @sub_todo_item_1.title,
-                state: :in_progress,
-                deadline: @sub_todo_item_1.deadline.to_datetime,
-                comment: @sub_todo_item_1.comment,
-              },
-              {
-                id: @sub_todo_item_2.id.to_s,
-                title: @sub_todo_item_2.title,
-                state: :in_progress,
-                deadline: @sub_todo_item_2.deadline.to_datetime,
-                comment: @sub_todo_item_2.comment,
-              }
-            ]
-          }
-        ]
-      },
-      result
-    )
+    assert_equal 1, result[:items].size
+    assert_equal 2, result[:items][0][:sub_todos].length
+    sub_todos = result[:items][0][:sub_todos].sort_by { |item| item[:id] }
+    assert_equal @sub_todo_item_1[:id], sub_todos[0][:id].to_i
   end
 end
