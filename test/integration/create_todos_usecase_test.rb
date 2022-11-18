9acf9d3a3bfc477dd1b3e1d004527986c9fe996e
snozaki <snozaki@netprotections.co.jp>
2022-11-18 20:36:59 +0900

    ADD: todo追加ユースケースのテスト追加

---
+++ test/integration/create_todos_usecase_test.rb
@@ -0,0 +1,36 @@
+require "test_helper"
+
+class CreateTodosUsecaseTest < ActionDispatch::IntegrationTest
+  def setup
+    @use_case = Todos::Usecases::CreateTodosUsecase.new
+  end
+
+  test "should throws error by an empty user" do
+    req = {
+      user_id: "0",
+      title: "review",
+      state: "0",
+      deadline: "2022-11-22",
+      comment: ""
+    }
+    assert_raises(Exceptions::ResourceNotFoundError) { @use_case.execute(req) }
+  end
+
+  test "should create a todo" do
+    user_michael = users(:michael)
+    req = {
+      user_id: user_michael.id,
+      title: "review",
+      state: "0",
+      deadline: "2022-11-22",
+      comment: ""
+    }
+
+    result = @use_case.execute(req)
+
+    assert_instance_of Todos::Todo, result
+    assert_equal req[:title], result.title.value
+    assert result.id.present?
+    assert TodoItem.where(id: result.id, user_id: user_michael.id).exists?
+  end
+end
