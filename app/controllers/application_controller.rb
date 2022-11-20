363e976aa8104bba946847fe8afce0c466aedcbd
snozaki <snozaki@netprotections.co.jp>
2022-11-20 21:40:26 +0900

    ADD: 共通エラーハンドラ

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,22 @@
 class ApplicationController < ActionController::API
+  include Exceptions
+
+  rescue_from StandardError, with: :to_internal_server_error
+  rescue_from ObjectValidationError, with: :to_bad_request
+  rescue_from ResourceNotFoundError, with: :to_bad_request
+  rescue_from ResourceUndefinedError, with: :to_bad_request
+
+  private
+    def to_bad_request
+      to_response(400)
+    end
+
+    def to_internal_server_error
+      to_response(500)
+    end
+
+    # @param [Integer] status
+    def to_response(status)
+      render json: "", status: status
+    end
 end
--- lib/exceptions.rb
+++ lib/exceptions.rb
@@ -1,6 +1,18 @@
 module Exceptions
   # General runtime exception for some resource(s) not found
-  class ResourceNotFoundError < StandardError; end
-  class ResourceUndefinedError < StandardError; end
-  class ObjectValidationError < StandardError; end
+  class ResourceNotFoundError < StandardError
+    def initialize(msg = "")
+      super
+    end
+  end
+  class ResourceUndefinedError < StandardError
+    def initialize(msg = "")
+      super
+    end
+  end
+  class ObjectValidationError < StandardError
+    def initialize(msg = "")
+      super
+    end
+  end
 end
