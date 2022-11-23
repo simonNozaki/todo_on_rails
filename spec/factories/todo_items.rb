6f12517cf270f5c557841b3f0ed07e17a7482c3f
snozaki <snozaki@netprotections.co.jp>
2022-11-23 22:25:58 +0900

    ファクトリ壊れてる(アソシエーションに合わせてID削除)

--- spec/factories/todo_items.rb
+++ spec/factories/todo_items.rb
@@ -1,9 +1,8 @@
 FactoryBot.define do
   factory :todo_item do
-    id { 1 }
     title { 'create a mtg handout' }
     state { '1' }
     deadline { 7.days.after }
-    user
+    association :user
   end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,6 @@ require 'faker'
 
 FactoryBot.define do
   factory :user do
-    id { 1 }
     name { Faker::Name.unique.name }
   end
 end
