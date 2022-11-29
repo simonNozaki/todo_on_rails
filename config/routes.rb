e178aaec9a0a20fdb786909603aa49ff79478ecf
snozaki <snozaki@netprotections.co.jp>
2022-11-29 21:05:49 +0900

    MOD: リソースの定義を、todosからusersに移し替え・テストの修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :healthchecks, only: [:index]
+  resources(:healthchecks, { only: [:index] })
   put('v1/todos/complete', { to: 'v1/todos#complete' })
-  namespace :v1 do
-    resources :todos, only: [:create, :update, :destroy, :show]
+  namespace(:v1) do
+    resources(:todos, { only: [:create, :update, :destroy] })
+    resources(:users, { only: [:show] })
   end
   # For routing not found
-  get '*not_found', to: 'application#handle_routing_error'
+  get('*not_found', { to: 'application#handle_routing_error' })
 end
